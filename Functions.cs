242840f3c0908bd7d5cf3919d747a05f2c87d946
Xavier <131487595+Omssiah@users.noreply.github.com>
2023-04-21 22:08:11 +0200

    Functions Nr 1.

---
+++ Functions.cs
@@ -0,0 +1,19 @@
+CLOSE BUTTON: 
+
+Application.Exit();
+
+---------------------
+
+MINIMIZE BUTTON:
+
+WindowState = FormWindowState.Minimized;
+
+---------------------
+OPEN FILE
+
+
+OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.Title = "Open";
+            }
